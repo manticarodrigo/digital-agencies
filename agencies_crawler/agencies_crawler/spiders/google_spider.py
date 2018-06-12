8f9c01eb1c71fed345c640b7f417d153eeec759a
Rodrigo Mantica <manticarodrigo@gmail.com>
2018-06-12 12:40:24 -0600

    added google_spider

---
+++ agencies_crawler/agencies_crawler/spiders/google_spider.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import json
+from urllib import parse
+import scrapy
+
+BASE_URL = 'https://partners.clients6.google.com/v2/companies?'
+PARAMS = {
+    'view': 'CV_GOOGLE_PARTNER_SEARCH',
+    'key': 'AIzaSyCi4oosxbZoHR65JXdMVy7eVRVsIO6tPlQ'
+}
+
+
+class GooglePartnersSpider(scrapy.Spider):
+    name = 'google_partners'
+    start_urls = [
+        BASE_URL + parse.urlencode(PARAMS)
+    ]
+
+    def parse(self, response):
+        json_data = json.loads(response.text)
+        print(json_data)
+        next_page_token = json_data['nextPageToken']
+        while next_page_token is not None:
+            print(next_page_token)
+            PARAMS['pageToken'] = next_page_token
+            yield response.follow(BASE_URL + parse.urlencode(PARAMS), callback=self.parse)
